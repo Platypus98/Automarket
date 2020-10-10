40da641234e7252802bafebe5471fa58d5596f3f
Голышков Илья Алексеевич <Golyshkov.I.A@sberbank.ru>
2020-10-10 10:36:20 +0300

    Initial Commit

---
+++ Automarket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Automarket
+//
+//  Created by 17815062 on 10.10.2020.
+//  Copyright © 2020 17815062. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ AutomarketTests/AutomarketTests.swift
@@ -0,0 +1,34 @@
+//
+//  AutomarketTests.swift
+//  AutomarketTests
+//
+//  Created by 17815062 on 10.10.2020.
+//  Copyright © 2020 17815062. All rights reserved.
+//
+
+import XCTest
+@testable import Automarket
+
+class AutomarketTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
